b4a9dada6ef594035ac9e488eedc4df70a068932
lgtkgtv <lgtkgtv@gmail.com>
2024-03-03 06:50:49 -0800

    Completed initial testing

--- file_actions.py
+++ file_actions.py
@@ -1,6 +1,8 @@
 import os
 import re
 from action_removing_duplicates import remove_duplicates
+from prettytable import PrettyTable
+
 
 def sanitize_filename(files):
     new_files = []
@@ -8,16 +10,30 @@ def sanitize_filename(files):
         new_filename = re.sub(r'[^\w\-_. ]', '', os.path.basename(file))
         new_filename = re.sub(r'\s+', ' ', new_filename).strip()
         new_filepath = os.path.join(os.path.dirname(file), new_filename)
-        os.rename(file, new_filepath)
-        print(f"Renamed '{file}' to '{new_filepath}'")
+        if new_filepath != file:
+            os.rename(file, new_filepath)
+            print(f"Renamed '{file}' to '{new_filepath}'")
         new_files.append(new_filepath)
     return new_files
+
+
 
 def list_files(files):
     """Print sorted list of filenames and filepaths."""
     sorted_files = sorted(files)
+    table = PrettyTable(["Filename", "Filepath"])
+    # Set the alignment to left
+    table.align["Filename"] = "l"
+    table.align["Filepath"] = "l"
+
     for file in sorted_files:
-        print(f"Filename: {os.path.basename(file)}, Filepath: {os.path.abspath(file)}")
+        filename = os.path.basename(file)
+        filepath = os.path.abspath(file)
+        # Add a row to the table for each file
+        table.add_row([filename, filepath])
+
+    # Print the table
+    print(table)
 
 def remove_files_with_zero_length(files):
     """Remove files with zero length."""
--- read_arguments.py
+++ read_arguments.py
@@ -29,29 +29,62 @@ Example usages:
     parser = argparse.ArgumentParser(description=f'''This program will perform a series of actions on all files selected based on comma separated input list of filename-patterns and directories. Optionally, it supports inputs from a YAML configuration file.
         {example_config}''', formatter_class=argparse.RawTextHelpFormatter)
     # parser = argparse.ArgumentParser(description=f"Utility to perform a series of actions on files selected based on a list of filename patterns and directories provided via YAML configuration file. {example_config}")
+    """
     parser.add_argument("-f", "--file_patterns", type=str, default="*", help="Comma-separated list of filename patterns to search for (default: *)")
     parser.add_argument("-d", "--dir_patterns", type=str, default=".", help="Comma-separated list of directory patterns to search in (default: .)")
     parser.add_argument("-a", "--actions", type=str, default="list_files", choices=["sanitize_filename", "list_files", "remove_files_with_zero_length", "remove_duplicates"], help="Comma-separated list of actions to perform on matching files (default: list_files)")
+    """
+    parser.add_argument("-f", "--file_patterns", type=str, help="Comma-separated list of filename patterns to search for (default: *)")
+    parser.add_argument("-d", "--dir_patterns", type=str, help="Comma-separated list of directory patterns to search in (default: .)")
+    parser.add_argument("-a", "--actions", type=str, choices=["sanitize_filename", "list_files", "remove_files_with_zero_length", "remove_duplicates"], help="Comma-separated list of actions to perform on matching files (default: list_files)")
+
     parser.add_argument("-c", "--config", help="path for YAML configuration file to read input arguments")
     parser.add_argument('-v', '--verbose', action='store_true',help='enable verbose mode')
     args = parser.parse_args()
 
+    if args.verbose:
+        print()
+        print("Command line Arguments:")
+        pprint.pprint(vars(args),indent=4)
+        print()
+
+    # Default configuration to use when YAML configuration is not provided 
+    yamlcfg_file_patterns = '*'
+    yamlcfg_dir_patterns = '.'
+    yamlcfg_actions = 'list_files'
     # If a configuration file was provided, read it and use its values
     if args.config:
-        with open(args.config, 'r') as f:
-            config = yaml.safe_load(f)
-        default_file_patterns = config.get('file_patterns', '*')
-        default_dir_patterns = config.get('dir_patterns', '.')
-        default_actions = config.get('actions', 'list_files')
-    else:
-        default_file_patterns = '*'
-        default_dir_patterns = '.'
-        default_actions = 'list_files'
+        try:
+            with open(args.config, 'r') as f:
+                config = yaml.safe_load(f)
 
-    # Override the configuration values with the command-line arguments if they are not the default values
-    args.file_patterns = args.file_patterns if args.file_patterns != '*' else default_file_patterns
-    args.dir_patterns = args.dir_patterns if args.dir_patterns != '.' else default_dir_patterns
-    args.actions = args.actions if args.actions != 'list_files' else default_actions
+                yamlcfg_file_patterns = config.get('file_patterns', ['*'])
+                yamlcfg_dir_patterns = config.get('dir_patterns', ['.'])
+                yamlcfg_actions = config.get('actions', ['list_files'])
+
+                if args.verbose:
+                    print(f"Configuration read from the YAML:")
+                    print(f"\tFile patterns: {yamlcfg_file_patterns}")
+                    print(f"\tDirectory patterns: {yamlcfg_dir_patterns}")
+                    print(f"\tActions: {yamlcfg_actions}")
+                    print()
+
+        except FileNotFoundError:
+            print(f"Error: The file {args.config} was not found.")
+            config = {}
+        except yaml.YAMLError as e:
+            print(f"Error: There was a problem with the format of the YAML file: {e}")
+            config = {}
+
+    # Override the YAML configuration and defaults with the command-line arguments if and when they are specified 
+    args.file_patterns = args.file_patterns if args.file_patterns is not None else yamlcfg_file_patterns
+    args.dir_patterns = args.dir_patterns if args.dir_patterns is not None else yamlcfg_dir_patterns
+    args.actions = args.actions if args.actions is not None else yamlcfg_actions
+
+    if args.verbose:
+        print("Arguments after YAML configuration override checks:")
+        pprint.pprint(vars(args),indent=4)
+        print()
 
     # Split the file_patterns, dir_patterns, and actions arguments into lists
     args.file_patterns = args.file_patterns.split(',') if isinstance(args.file_patterns, str) else args.file_patterns
@@ -62,6 +95,6 @@ Example usages:
     if args.verbose:
         print("Arguments provided:")
         pprint.pprint(vars(args),indent=4)
-        print()
+        print("-------------------------------------------------------------")
 
     return args
