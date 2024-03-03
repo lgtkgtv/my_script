7e85025d94a4ee198fed67d3d43da331e434c494
lgtkgtv <lgtkgtv@gmail.com>
2024-03-03 08:00:08 -0800

    added .gitignore

--- input_validation.py
+++ input_validation.py
@@ -6,9 +6,10 @@ import pprint
 IGNORE_RESPONSE = 'n'
 INVALID_FILENAME_CHARS = ['<', '>', ':', '"', '/', '\\', '|']  # Allow wildcards '?' and '*' as valid chars
 INVALID_DIRECTORY_CHARS = ['<', '>', ':', '"', '|']
+VALID_ACTIONS = ["sanitize_filename", "list_files", "remove_files_with_zero_length", "remove_duplicates"]
 
 if platform.system() == 'Windows':
-    SAFE_DIRECTORIES = [
+    OS_ENV_DIRECTORIES = [
         'C:\\Windows\\System32', 
         'C:\\Program Files', 
         'C:\\ProgramData', 
@@ -16,7 +17,7 @@ if platform.system() == 'Windows':
         'C:\\'
     ]
 elif platform.system() == 'Darwin':  # MacOS
-    SAFE_DIRECTORIES = [
+    OS_ENV_DIRECTORIES = [
         '/usr/local', 
         '/usr/bin', 
         '/Applications', 
@@ -26,7 +27,7 @@ elif platform.system() == 'Darwin':  # MacOS
         '/'
     ]
 else:  # Linux and other Unix-like systems
-    SAFE_DIRECTORIES = [
+    OS_ENV_DIRECTORIES = [
         '/usr/local', 
         '/usr/bin', 
         '/etc', 
@@ -49,19 +50,42 @@ def is_valid_filename(filename):
     """Check if a filename is valid."""
     for char in filename:
         if char in INVALID_FILENAME_CHARS:
-            return False, f"The filename {filename} contains invalid characters: {char}"
+            return False, f"The filename '{filename}' contains invalid characters: '{char}'"
     return True, ""
 
 def is_valid_directory(directory):
     """Check if a directory is valid."""
     for char in directory:
         if char in INVALID_DIRECTORY_CHARS:
-            return False, f"The directory {directory} contains invalid characters: {char}"
+            return False, f"The directory '{directory}' contains invalid characters: '{char}'"
     return True, ""
 
 def is_safe_directory(directory):
-    """Check if a directory is safe."""
-    return directory not in SAFE_DIRECTORIES, f"The directory {directory} is not safe."
+    """Check if a directory is safe.
+
+    The function checks whether the provided `directory` is not in a predefined list or set named `OS_ENV_DIRECTORIES`. 
+    This list contains directories that are considered "unsafe" because they contain OS or other sensitive files.
+
+    The function returns a tuple. 
+        The first element of the tuple is a boolean value that is `True` if the directory is not in `OS_ENV_DIRECTORIES`, and `False` otherwise. 
+
+        The second element of the tuple is a string that provides a message indicating whether the directory is allowed or not.
+        If the directory is in `OS_ENV_DIRECTORIES`, 
+            the function returns `(False, "The directory 'directory' has OS or other sensitive files and not allowed.")`. 
+    """
+    return directory not in OS_ENV_DIRECTORIES, f"The directory '{directory}' has OS or other sensative files and is unsafe"
+
+def is_valid_action(action):
+    """Check if the action is allowed
+    The function returns a tuple. 
+        The first element of the tuple is a boolean value that is `True` if the action is in `VALID_ACTIONS`, and `False` otherwise. 
+
+        The second element of the tuple is a string that provides a message indicating whether the action is allowed or not.
+        If the action is not in `VALID_ACTIONS`, 
+            the function returns `(False, "The action 'action' is unknown and invalid.")`.
+    """
+    return action in VALID_ACTIONS, f"The action '{action}' is unknown and invalid."
+
 
 def validate_patterns(patterns, validation_func):
     """Validate a list of patterns using a validation function."""
@@ -81,13 +105,13 @@ def validate_patterns(patterns, validation_func):
 
 
 
-
 def input_validation(args):
     """Perform input validations."""
 
     args.file_patterns = validate_patterns(args.file_patterns, is_valid_filename)
     args.dir_patterns = validate_patterns(args.dir_patterns, is_valid_directory)
     args.dir_patterns = validate_patterns(args.dir_patterns, is_safe_directory)
+    args.actions = validate_patterns(args.actions, is_valid_action)
 
     if len(args.file_patterns) == 0:
         raise ValidationError("At least one valid file pattern is required.")
--- read_arguments.py
+++ read_arguments.py
@@ -28,25 +28,12 @@ Example usages:
     """
     parser = argparse.ArgumentParser(description=f'''This program will perform a series of actions on all files selected based on comma separated input list of filename-patterns and directories. Optionally, it supports inputs from a YAML configuration file.
         {example_config}''', formatter_class=argparse.RawTextHelpFormatter)
-    # parser = argparse.ArgumentParser(description=f"Utility to perform a series of actions on files selected based on a list of filename patterns and directories provided via YAML configuration file. {example_config}")
-    """
-    parser.add_argument("-f", "--file_patterns", type=str, default="*", help="Comma-separated list of filename patterns to search for (default: *)")
-    parser.add_argument("-d", "--dir_patterns", type=str, default=".", help="Comma-separated list of directory patterns to search in (default: .)")
-    parser.add_argument("-a", "--actions", type=str, default="list_files", choices=["sanitize_filename", "list_files", "remove_files_with_zero_length", "remove_duplicates"], help="Comma-separated list of actions to perform on matching files (default: list_files)")
-    """
     parser.add_argument("-f", "--file_patterns", type=str, help="Comma-separated list of filename patterns to search for (default: *)")
     parser.add_argument("-d", "--dir_patterns", type=str, help="Comma-separated list of directory patterns to search in (default: .)")
     parser.add_argument("-a", "--actions", type=str, choices=["sanitize_filename", "list_files", "remove_files_with_zero_length", "remove_duplicates"], help="Comma-separated list of actions to perform on matching files (default: list_files)")
-
     parser.add_argument("-c", "--config", help="path for YAML configuration file to read input arguments")
     parser.add_argument('-v', '--verbose', action='store_true',help='enable verbose mode')
     args = parser.parse_args()
-
-    if args.verbose:
-        print()
-        print("Command line Arguments:")
-        pprint.pprint(vars(args),indent=4)
-        print()
 
     # Default configuration to use when YAML configuration is not provided 
     yamlcfg_file_patterns = '*'
@@ -62,12 +49,13 @@ Example usages:
                 yamlcfg_dir_patterns = config.get('dir_patterns', ['.'])
                 yamlcfg_actions = config.get('actions', ['list_files'])
 
+                """
                 if args.verbose:
                     print(f"Configuration read from the YAML:")
                     print(f"\tFile patterns: {yamlcfg_file_patterns}")
                     print(f"\tDirectory patterns: {yamlcfg_dir_patterns}")
                     print(f"\tActions: {yamlcfg_actions}")
-                    print()
+                """
 
         except FileNotFoundError:
             print(f"Error: The file {args.config} was not found.")
@@ -81,20 +69,15 @@ Example usages:
     args.dir_patterns = args.dir_patterns if args.dir_patterns is not None else yamlcfg_dir_patterns
     args.actions = args.actions if args.actions is not None else yamlcfg_actions
 
-    if args.verbose:
-        print("Arguments after YAML configuration override checks:")
-        pprint.pprint(vars(args),indent=4)
-        print()
-
     # Split the file_patterns, dir_patterns, and actions arguments into lists
     args.file_patterns = args.file_patterns.split(',') if isinstance(args.file_patterns, str) else args.file_patterns
     args.dir_patterns = args.dir_patterns.split(',') if isinstance(args.dir_patterns, str) else args.dir_patterns
     args.actions = args.actions.split(',') if isinstance(args.actions, str) else args.actions    
-
+    """
     # Print all arguments
-    if args.verbose:
-        print("Arguments provided:")
+        if args.verbose:
+        print("Final arguments after processing YAML config overrides and defaults:")
         pprint.pprint(vars(args),indent=4)
-        print("-------------------------------------------------------------")
-
+        print()
+    """
     return args
